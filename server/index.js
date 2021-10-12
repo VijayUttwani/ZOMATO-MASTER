824cfbcc2734bc2f320380dd2a6d384a73810d4c
VijayUttwani <vijaykumaruttwani@gmail.com>
2021-10-12 23:04:01 +0530

    Established Connection with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
+require("dotenv").config();
+
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+// API
+import Auth from "./API/Auth"
+
+// Database Connection
+import ConnectDB from "./database/connection";
 
 const zomato = express();
 
@@ -10,6 +18,14 @@ zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
 
+// For Application Routes
+// Example Route: localhost:4000/auth/signup
+zomato.use("/auth", Auth)
+
 zomato.get("/", (req, res) => res.json({ message: "SetUp Success🎉" }));
 
-zomato.listen(4000, () => console.log("Server is up and running🚀"));
+zomato.listen(4000, () =>
+	ConnectDB()
+		.then(() => console.log("Server is up and running🚀"))
+		.catch(() => console.log("DB Connection Failed"))
+);
