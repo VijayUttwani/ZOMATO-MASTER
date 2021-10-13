21d63f4084480df3d0a3732d75a73aca98108e78
VijayUttwani <vijaykumaruttwani@gmail.com>
2021-10-13 14:07:35 +0530

    Added Food API & Route

---
+++ server/API/Food/index.js
@@ -0,0 +1,49 @@
+// Libraries
+import express from "express";
+import passport from "passport";
+
+// Database Model
+import { FoodModel } from "../../database/food";
+
+const Router = express.Router();
+
+/*
+Route     /
+Des       Get all the food based on restaurants
+Params    _id
+Access    Public
+Method    GET  
+*/
+
+Router.get("/restaurant:_id", async (req, res) => {
+	try {
+		const { _id } = req.params;
+		const foods = await FoodModel.find({ restaurant: _id });
+
+		return res.json({ foods });
+	} catch (error) {
+		return res.status(500).json({ error: error.message });
+	}
+});
+
+/*
+Route     /category/:category
+Des       Get all the food based on category
+Params    category
+Access    Public
+Method    GET  
+*/
+
+Router.get("/category/:category")
+try {
+	const { category } = req.params;
+	const foods = await FoodModel.find({
+		category: { $regex: category, $options: "i" },
+	});
+
+	return res.json({ foods });
+} catch (error) {
+	return res.json(500).json({ error: error.message });
+}
+
+export default Router;
--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import googleAuthConfig from "./config/google.config";
 // API
 import Auth from "./API/Auth";
 import Restaurant from "./API/Restaurant"
+import Food from "./API/Food"
 
 // Database Connection
 import ConnectDB from "./database/connection";
@@ -31,7 +32,8 @@ googleAuthConfig(passport);
 // For Application Routes
 // Example Route: localhost:4000/auth/signup
 zomato.use("/auth", Auth);
-zomato.use("./restaurant, Restaurant");
+zomato.use("./restaurant", Restaurant);
+zomato.use("./food", Food);
 
 
 zomato.get("/", (req, res) => res.json({ message: "SetUp Success🎉" }));
