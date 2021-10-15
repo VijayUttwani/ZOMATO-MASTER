48b8d4386e61f59f0b78ec936f3f941b58e3927b
VijayUttwani <vijaykumaruttwani@gmail.com>
2021-10-15 17:47:14 +0530

    Added Restaurant Card

--- client/src/Components/Delivery/Brand.jsx
+++ client/src/Components/Delivery/Brand.jsx
@@ -6,13 +6,13 @@ import { NextArrow, PrevArrow } from "../CarousalArrow";
 
 const Brand = (props) => {
 	const logos = [
-		"https://b.zmtcdn.com/data/brand_creatives/logos/1a985408ca7ad8fd097f2c47db9c5cb6_1611252598.png?output-format=webp",
+		"https://b.zmtcdn.com/data/brand_creatives/logos/80c09d718acddee05a655eb378bb442f_1617875125.png?output-format=webp",
 		"https://b.zmtcdn.com/data/brand_creatives/logos/c38f7540bcc5a38e918856ac06409056_1504531339.png?output-format=webp",
 		"https://b.zmtcdn.com/data/brand_creatives/logos/6a11fd0f30c9fd9ceaff2f5b21f61d23_1617187721.png?output-format=webp",
-		"https://b.zmtcdn.com/data/brand_creatives/logos/7a7904eff687a7456599074a4e7d2335_1617875100.png?output-format=webp",
-		"https://b.zmtcdn.com/data/brand_creatives/logos/ec72c289aa01d947fe15a009ce8e559f_1616052816.png?output-format=webp",
+		"https://b.zmtcdn.com/data/brand_creatives/logos/1a985408ca7ad8fd097f2c47db9c5cb6_1611252598.png?output-format=webp",
 		"https://b.zmtcdn.com/data/brand_creatives/logos/874c2b2b4554f4aed7dd3bb4e755c420_1604383019.png?output-format=webp",
-		"https://b.zmtcdn.com/data/brand_creatives/logos/80c09d718acddee05a655eb378bb442f_1617875125.png?output-format=webp",
+		"https://b.zmtcdn.com/data/brand_creatives/logos/a7d28fb80d54066761f3d46c17168274_1617972125.png?output-format=webp",
+		"https://b.zmtcdn.com/data/brand_creatives/logos/948574cd50798ba77c066b93e11b0e24_1617972221.png?output-format=webp",
 	];
 	const settings = {
 		arrows: true,
@@ -21,24 +21,22 @@ const Brand = (props) => {
 		slidesToShow: 6,
 		slidesToScroll: 1,
 		nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        responsive: [
-            {
+		prevArrow: <PrevArrow />,
+		responsive: [
+			{
 				breakpoint: 1024,
-                settings: {
+				settings: {
 					slidesToShow: 4,
-                    slidesToScroll: 3,
-                    
+					slidesToScroll: 3,
 				},
-            },
+			},
 			{
 				breakpoint: 480,
-                settings: {
+				settings: {
 					slidesToShow: 2,
 					slidesToScroll: 1,
 				},
-            },
-            
+			},
 		],
 	};
 	return (
@@ -47,12 +45,8 @@ const Brand = (props) => {
 			<div className="my-5">
 				<Slider {...settings}>
 					{logos.map((logo) => (
-						<div className="w-32 h-48 mx-10 bg-white">
-							<img
-								src={logo}
-								alt="brand"
-								className="w-32 h-32"
-							/>
+						<div className="w-32 h-32 mx-10 bg-white">
+							<img src={logo} alt="brand" className="w-32 h-32" />
 						</div>
 					))}
 				</Slider>
--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -1,16 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 import DeliveryCarousal from "./DeliveryCarousal";
 import Brand from "./Brand";
+import RestaurantCard from "./RestaurantCard";
 
 const Delivery = () => {
-  return (
-    <>
-          <DeliveryCarousal />
-          <Brand />
-    </>
-  );
+	const [restaurantList, setRestaurantList] = useState([
+		{
+			_id: "123456",
+			photos: [
+				"https://b.zmtcdn.com/data/pictures/chains/3/1401863/930ee0cc8be9a105207904e0979904b3_o2_featured_v2.jpg?output-format=webp",
+			],
+			name: "Vijayshree Sandwich",
+			cuisine: ["Fast Food", "Beverages", "Sandwich"],
+			averageCost: 200,
+			isPro: true,
+			isOff: 80,
+			durationOfdelivery: 25,
+			restaurantReviewValue: 4.3,
+        },
+        {
+			_id: "123456-2",
+			photos: [
+				"https://b.zmtcdn.com/data/pictures/chains/3/18343993/c0fba8fbbd5bdac58e0e5d3b277a5cd4_o2_featured_v2.jpg?output-format=webp",
+			],
+			name: "Behrouz Biryani",
+			cuisine: ["Biryani", "North Indian", "Mughlai"],
+			averageCost: 350,
+			isPro: true,
+			isOff: 80,
+			durationOfdelivery: 47,
+			restaurantReviewValue: 4.7,
+        },
+		{
+			_id: "123456-3",
+			photos: [
+				"https://b.zmtcdn.com/data/pictures/5/1400315/d464789d84c842dfc3f6c5c1d283f361_o2_featured_v2.jpg?output-format=webp",
+			],
+			name: "Johny Hot Dog",
+			cuisine: ["Fast Food", "Hot Dog", "Egg-Benjo"],
+			averageCost: 30,
+			isPro: true,
+			isOff: 80,
+			durationOfdelivery: 30,
+			restaurantReviewValue: 5,
+        },
+	]);
+
+	return (
+		<>
+			<DeliveryCarousal />
+			<Brand />
+			<div className="flex justify-between flex-wrap">
+				{restaurantList.map((restaurant) => (
+					<RestaurantCard {...restaurant} key={restaurant._id} />
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Delivery;
