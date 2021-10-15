5daf5993c1294c130b0a74e724108e5c20347e64
VijayUttwani <vijaykumaruttwani@gmail.com>
2021-10-15 18:31:33 +0530

    Added NightLife Carousel

---
+++ client/src/Components/NightLife/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import NightLifeCarousal from "./NightLifeCarousal";
+
+const NightLife = () => {
+	return (
+		<div>
+			<NightLifeCarousal />
+		</div>
+	);
+};
+
+export default NightLife;
