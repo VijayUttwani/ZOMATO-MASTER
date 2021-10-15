fac2d45d36d72fad3d8efa43bb1e17789553919f
VijayUttwani <vijaykumaruttwani@gmail.com>
2021-10-15 14:13:46 +0530

    Added Brand Carousel

--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 // components
 import DeliveryCarousal from "./DeliveryCarousal";
+import Brand from "./Brand";
 
 const Delivery = () => {
   return (
     <>
-      <DeliveryCarousal />
+          <DeliveryCarousal />
+          <Brand />
     </>
   );
 };
