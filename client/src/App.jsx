77fc4ad610203246a434f3c4548724dd6a990fdd
VijayUttwani <vijaykumaruttwani@gmail.com>
2021-10-16 15:00:30 +0530

    Added Restaurant Tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,11 +13,32 @@ import Home from "./Page/Home";
 function App() {
 	return (
 		<>
-			<Route path ="/" exact>
-				<Redirect to ="/delivery" />
+			<Route path="/" exact>
+				<Redirect to="/delivery" />
 			</Route>
 			<HomeLayoutHOC path="/:type" exact component={Home} />
 			<RestaurantLayoutHOC path="/:restaurant/:id" exact component={Temp} />
+			<RestaurantLayoutHOC
+				path="/restaurant/:id/overview"
+				exact
+				component={Temp}
+			/>
+			<RestaurantLayoutHOC
+				path="/restaurant/:id/order-online"
+				exact
+				component={Temp}
+			/>
+			<RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
+			<RestaurantLayoutHOC
+				path="/restaurant/:id/reviews"
+				exact
+				component={Temp}
+			/>
+			<RestaurantLayoutHOC
+				path="/restaurant/:id/photos"
+				exact
+				component={Temp}
+			/>
 		</>
 	);
 }
